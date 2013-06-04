68d185d65db43261d79cbfe43102c9e7bd648310
cherimarie <cherimarie@gmail.com>
2013-06-04 14:16:33 -0700

    Add JRE library to project and create new Task class

--- taskList/src/Task.java
+++ taskList/src/Task.java
@@ -1,16 +1,4 @@
-import java.util.ArrayList;
-
 
 public class Task {
-	String name;
-	int time;
-	int priority;
-	String reward;
-	
-	public Task(String name,int time,int priority,String reward){
-			this.name = name;
-			this.time = time;
-			this.priority = priority;
-			this.reward = reward;
-	}
+
 }
